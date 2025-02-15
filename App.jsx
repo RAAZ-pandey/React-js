74e03d1ca8ca624e5088a2b93a8982bbd191e3ad
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-02-15 23:22:42 +0530

    basic react

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+import Foo from "./Food.jsx";
+import Food from "./Food.jsx";
+
+function App() {
+    
+  return(
+    <>
+    <Header/>
+    <Food/>
+    <Footer/>
+    
+    </>
+  );
+}
+
+export default App
