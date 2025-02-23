437d1ccd3bf0fbe9de970ab12edecb95b79ffb93
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2025-02-23 22:55:01 +0530

    pageeffect

---
+++ PageEffect.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from "react";
+
+function PageEffect(){
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+    
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    console.log("Event Listener Added");
+
+    return() => {
+      window.removeEventListener("resize", handleResize);
+      console.log("Event Listener Removed");
+    }
+  }, []);
+
+  function handleResize(){
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
+  return(<>
+         <p>Window Width: {width}px</p>
+         <p>Window Height: {height}px</p>
+  </>)
+}
+
+export default  PageEffect;
